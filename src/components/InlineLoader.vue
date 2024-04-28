1d9282918a3e525149ebd099c48e04f0b7f636a1
Rui H L Oliveira <ruihloliveira@gmail.com>
2024-04-28 18:44:56 -0300

    novo inline loader

---
+++ src/components/InlineLoader.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div v-if="busy" class="inlineLoaderContainer">
+      <div>
+        Aguarde...
+      </div>
+      <div class="inlineLoader"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      ini: ''
+    }
+  },
+  props: {
+    busy: Boolean
+  }
+}
+</script>
+
+<style>
+.inlineLoaderContainer {
+  /* position: absolute; */
+  /* top: 0; */
+  /* left: 0; */
+  /* width: 100vw; */
+  /* height: 100vh; */
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 5px;
+  width: 140px;
+  height: 60px;
+  background: #e0e0e0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.inlineLoader {
+  border: 5px solid #f3f3f3; /* Light grey */
+  border-top: 5px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
